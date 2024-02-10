f0c89cfaa1daaa84e1aa1e2ae2cb1f4bf9e38114
Jake Kadir <jakekadir0@gmail.com>
2024-02-10 16:57:14 +0000

    frontend-feature: enable and create alarms

--- alarm_clock_frontend/src/App.js
+++ alarm_clock_frontend/src/App.js
@@ -1,54 +1,113 @@
 import React, { useState, useEffect } from 'react';
 import './App.css';
-
 const BACKEND_URL="http://127.0.0.1:8000/"
+const DEFAULT_ALARM={
+  schedule: ["30", "8", "*", "*", "*"],
+  enabled: true,
+}
+
+function AlarmObj(id, schedule, enabled){
+  return {
+    id: id, 
+    schedule: schedule,
+    enabled: enabled
+  }
+}
+
+async function sendRequest(route, method, body, setData){
+  try {
+    const response = await fetch(BACKEND_URL+route, {
+      method: method,
+      body: (body !== null)? JSON.stringify(body) : null,
+      headers: {
+        "Content-Type" : "application/json"
+      }
+    });
+    if(response.status === 200){
+      const result = await response.json();
+      setData(result);
+    }
+    else{
+      console.error("Error fetching data:", response);
+    }
+  } catch (error) {
+    console.error('Error fetching data:', error);
+  }
+};
+
+async function updateAlarm(alarm, setState){
+  await sendRequest("cron", "PUT", alarm, setState)
+}
+
+function EnableAlarm(props){
+  return (
+    <form>
+      <input type="checkbox" defaultChecked={props.enabled} onChange={props.onChange}/>
+    </form>
+  )
+}
+
+function CreateAlarm(props){
+  async function onClick(){
+    // send a request
+    sendRequest("cron", "POST", DEFAULT_ALARM, () => {});
+    // update list of alarms by getting all alarms
+    await sendRequest("crons", "GET", null, props.setData)
+  }
+  return (
+    <button type="button" onClick={onClick}>Add new alarm</button>
+  )
+}
 
 function Alarm(props){
-  console.log("id", props.id);
+  // const [enabled, setEnabled] = useState(props.enabled);
+  const [state, setState] = useState(props.alarm)
+  async function enabledChange(){
+    // change enabled state of alarm
+    updateAlarm({
+      id: state.id,
+      enabled: !state.enabled
+    }, setState);
+  }
   return (
-    <div className="Alarm" key={props.id}>
-      <p>id: {props.id}</p>
-      <p>schedule: {props.schedule.join(" ")}</p>
+    <div className="Alarm" key={"alarm" + props.id}>
+      <p>id: {state.id}</p>
+      <p>schedule: {state.schedule.join(" ")}</p>
+      <EnableAlarm id={state.id} enabled={state.enabled} onChange={enabledChange}/>
     </div>
   )
 }
 
-const Alarms = () => {
-  const [data, setData] = useState([]);
-
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const response = await fetch(BACKEND_URL+"crons");
-        const result = await response.json();
-        setData(result);
-      } catch (error) {
-        console.error('Error fetching data:', error);
-      }
-    };
-
-    fetchData();
-  }, []);
-
+function Alarms(props){
   return (
     <div>
-      {data.map(alarm => {
-        return(
-          <Alarm key={alarm.id} id={alarm.id} schedule={alarm.schedule}/>
-        );
-      })}
+      {
+        props.alarms.map(alarm =>
+          <Alarm key={alarm.id} alarm={alarm}/>
+        )
+      }
     </div>
   )
 }
 
 function App() {
+  const [alarms, setAlarms] = useState([]);
+  useEffect(() => {
+    sendRequest("crons", "GET", null, setAlarms);
+  }, []);
+
   return (
     <div className="App" key="app">
       <header className="App-header" key="header">
-        <Alarms/>
+        ALARM CLOCKS
+        <div>
+          <Alarms alarms={alarms}/>
+          <CreateAlarm setData={setAlarms}/>
+        </div>
       </header>
     </div>
   );
+
 
 }
 
